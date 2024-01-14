afe9aaa53ef9fe5055e43156e946c7c36406de32
TM-4 <156091993+TM-4@users.noreply.github.com>
2024-01-14 23:03:08 +0900

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,10 @@
+'use strict';
+{
+    const btn = document.querySelector('.btn');
+    const container = document.querySelector('.container');
+
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('active');
+        container.classList.toggle('active');
+    });
+}
